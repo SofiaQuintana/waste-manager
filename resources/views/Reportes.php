e7e8ff65d126a754bb083d06b34f1270bb24a4d9
AlanMT <moralesalan235@gmail.com>
2023-11-11 19:20:44 -0600

    reportes

--- resources/views/Reportes.php
+++ resources/views/Reportes.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inventario</title>
+    <title>Reportes</title>
     <link rel="stylesheet" href="/resources/css/invetari.css">
 
 
@@ -23,7 +23,7 @@
                 <li><a href="#">
                         <span class="item">Principal</span>
                     </a></li>
-                    <li><a href="/resources//views/inventario.php">
+                <li><a href="/resources//views/inventario.php">
                         <span class="item">Ver inventario</span>
                     </a></li>
                 <li><a href="/resources//views/Reportes.php">
@@ -45,35 +45,45 @@
                 </div>
             </div>
         </div>
+
+        <div class="filtrado">
+                <form action="">
+                    <div class="row">
+                            <input type="date" name="fecha_ingreso" class="form-control" placeholder="Fecha de Inicio" required>
+                            <input type="date" name="fechaFin" class="form-control" placeholder="Fecha Final" required>
+                            <input type="submit" value="Filtrar" class="btn">
+                    </div>
+                </form>
+        </div>
         <div class="tablas">
             <table>
                 <thead>
                     <tr>
-                        <th>Id</th>
+                        <th>id</th>
                         <th>Nombre</th>
-                        <th>Cantidad</th>
+                        <th>Monto</th>
                         <th>fecha</th>
                     </tr>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Chatarra</td>
-                            <td>5kg</td>
-                            <td>2023-05-02</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Bolsas</td>
-                            <td>1kg</td>
-                            <td>2023-01-03</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>botellas</td>
-                            <td>2kg</td>
-                            <td>2023-02-02</td>
-                        </tr>
-                    </tbody>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Chatarra</td>
+                        <td>5kg</td>
+                        <td>2023-05-02</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Bolsas</td>
+                        <td>1kg</td>
+                        <td>2023-01-03</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>botellas</td>
+                        <td>2kg</td>
+                        <td>2023-02-02</td>
+                    </tr>
+                </tbody>
                 </thead>
             </table>
         </div>
