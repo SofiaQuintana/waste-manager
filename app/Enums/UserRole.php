75a4a7da5aeaf8c3bf3b9aecc50ec829d7b045db
Zofia <39326905+SofiaQuintana@users.noreply.github.com>
2023-11-12 00:06:48 -0600

    feat: add db migration files (#2)
    * fix: updated wrong enum data type
    
    * feat: db migration files

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -1,12 +1,12 @@
 <?php
 namespace App\Enums;
 
-enum UserRole {
-    case Admin;
-    case Manager;
-    case Operator;
-    case WasteClassifier;
-    case Seller;
-    case User;
+enum UserRole: int {
+    case Admin = 1;
+    case Manager = 2;
+    case Operator = 3;
+    case WasteClassifier = 4;
+    case Seller = 5;
+    case User = 6;
     
 }
--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use App\Enums\RoleEnum;
+use App\Enums\UserRole as RoleEnum;
 use App\Models\WasteIncome;
 use App\Models\Conversions;
 use App\Models\Sells;
