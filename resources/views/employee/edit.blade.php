bcbfc93e34b8f84ec5045a1a4d4b59b1a6db5401
Randy Sum <randy26smc@gmail.com>
2023-11-13 20:04:55 -0600

    corregido el editar usuario

--- resources/views/employee/edit.blade.php
+++ resources/views/employee/edit.blade.php
@@ -1,5 +1,5 @@
-<form action="url {{('employee/'.$employee->id)}}" method="POST">
+<form action="{{ url('/employee/'.$employee->id)}}" method="post">
     @csrf
-    {{method_field('PATCH')}}
-    @include('employee.form', ['editMode' => true])
+    {{ method_field('PATCH') }}
+    @include('employee.form', ['editMode' => true, 'mode' => 'Editar', 'employee' => $employee])
 </form>
