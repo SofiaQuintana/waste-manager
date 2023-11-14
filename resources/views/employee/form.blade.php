0475ff2ccb0e402b51b028c69078717421daa1aa
Zofia <39326905+SofiaQuintana@users.noreply.github.com>
2023-11-14 02:44:27 -0600

    Fix UI styles and templates (#11)
    * fix: moved header and its options to specific files
    
    * feat: added employee data to session
    
    * fix: moved and renamed css files
    
    * fix: updated employee crud views
    
    * fix: removed unused view

--- resources/views/employee/form.blade.php
+++ resources/views/employee/form.blade.php
@@ -1,80 +1,65 @@
 <!DOCTYPE html>
-<html lang="es">
-
-<head>
-    <!-- ...otros encabezados... -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-
-<body>
-    <div class="container">
-        
-
-        @if(count($errors)>0)
-        <div class="alert-danger" role="alert">
-            <ul>
-                @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-
-
-        @endif
-
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <form action="{{ route('employee.store') }}" method="post" enctype="multipart/form-data">
-                    @csrf
-
-                    <div class="form-group">
-                        <label for="username">Username</label>
-                        <input type="text" class="form-control" name="username" value="{{ isset($employee->username) ? $employee->username : '' }}" id="username">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="name">Nombre</label>
-                        <input type="text" class="form-control" name="name" value="{{ isset($employee->name) ? $employee->name : '' }}" id="name">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="last_name">Apellido</label>
-                        <input type="text" class="form-control" name="last_name" value="{{ isset($employee->last_name) ? $employee->last_name : '' }}" id="last_name">
-                    </div>
-
-                    @if(!$editMode)
-                    <div class="form-group">
-                        <label for="password">Contraseña</label>
-                        <input type="password" class="form-control" name="password" id="password">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="role">Rol</label>
-                        <select name="role" class="form-control">
-                            <option value="{{ \App\Enums\UserRole::Admin->value }}">Admin</option>
-                            <option value="{{ \App\Enums\UserRole::Manager->value }}">Manager</option>
-                            <option value="{{ \App\Enums\UserRole::Operator->value }}">Operator</option>
-                            <option value="{{ \App\Enums\UserRole::WasteClassifier->value }}">WasteClassifier</option>
-                            <option value="{{ \App\Enums\UserRole::Seller->value }}">Seller</option>
-                            <option value="{{ \App\Enums\UserRole::User->value }}">User</option>
-                        </select>
-                    </div>
-                    @endif
-
-                    <div class="form-group">
-                        <input type="submit" class="btn btn-primary" value="{{ $mode }} Datos">
-                    </div>
-
-                    <div class="form-group">
-                        <a href="{{ url('employee/') }}" class="btn btn-secondary">Regresar</a>
-                    </div>
-                </form>
-            </div>
-        </div>
+<html lang="en">
+  @if(count($errors)>0)
+    <div class="alert-danger" role="alert">
+        <ul>
+            @foreach($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
     </div>
+    @endif
 
-    <!-- Agrega scripts de Bootstrap al final del cuerpo del documento para mejorar el rendimiento -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
-</body>
+    <div class="Empleado">
+        <h1>Registro de Empleados</h1>
+        <form action="{{ route('employee.store') }}" method="post" enctype="multipart/form-data">
+            @csrf
 
-</html>
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" name="username" value="{{ isset($employee->username) ? $employee->username : '' }}" id="username">
+            </div>
+
+            <div class="form-group">
+                <label for="name">Nombre</label>
+                <input type="text" class="form-control" name="name" value="{{ isset($employee->name) ? $employee->name : '' }}" id="name">
+            </div>
+
+            <div class="form-group">
+                <label for="last_name">Apellido</label>
+                <input type="text" class="form-control" name="last_name" value="{{ isset($employee->last_name) ? $employee->last_name : '' }}" id="last_name">
+            </div>
+
+            @if(!$editMode)
+            <div class="form-group">
+                <label for="password">Contraseña</label>
+                <input type="password" class="form-control" name="password" id="password">
+            </div>
+
+            <div class="form-group">
+                <label for="role">Rol</label>
+                <br>
+                <select name="role" id="role" class="custom-select">
+                    <option value="{{ \App\Enums\UserRole::Admin->value }}">Admin</option>
+                    <option value="{{ \App\Enums\UserRole::Manager->value }}">Manager</option>
+                    <option value="{{ \App\Enums\UserRole::Operator->value }}">Operator</option>
+                    <option value="{{ \App\Enums\UserRole::WasteClassifier->value }}">WasteClassifier</option>
+                    <option value="{{ \App\Enums\UserRole::Seller->value }}">Seller</option>
+                    <option value="{{ \App\Enums\UserRole::User->value }}">User</option>
+                </select>
+            </div>
+            @endif
+
+            <br>
+            <div class="form-group">
+                <input type="submit" class="btn btn-dark" value="{{ $mode }} Datos">
+            </div>
+            <br>
+            <div class="form-group">
+                <a href="{{ url('employee/') }}" class="btn btn-dark">Regresar</a>
+            </div>
+        </form>
+    </div>
+    <div class="imagen">
+        <img src="https://www.traperosdeemaus.org/mini-slider/reciclaje/reciclaje-general.png" alt="">
+    </div>
--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -4,56 +4,52 @@
 <head>
     <!-- ...otros encabezados... -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/base.css') }}">
 </head>
 <body>
     <!-- ...contenido... -->
-
-    <!-- resources/views/employee/index.blade.php -->
-
-
-@if(Session::has('message'))
-    <div class="alert alert-success">
-        {{ Session::get('message') }}
+    <div class="wrapper">
+        <!-- resources/views/employee/index.blade.php -->
+        @include('layouts.header')
+        <div class="tablas">
+            @if (Session::has('message'))
+                <div class="alert alert-success">
+                    {{ Session::get('message') }}
+                </div>
+            @endif
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Username</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>Rol</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($employees as $employee)
+                        <tr>
+                            <td>{{ $employee->id }}</td>
+                            <td>{{ $employee->username }}</td>
+                            <td>{{ $employee->name }}</td>
+                            <td>{{ $employee->last_name }}</td>
+                            <td>{{ \App\Enums\UserRole::getDescription($employee->role->value) }}</td>
+                            <td>
+                                <a href="{{ url('employee/'.$employee->id.'/edit') }}" class="btn btn-sm btn-warning">Editar</a>
+                                <form action="{{ route('employee.destroy', $employee->id) }}" method="post" class="d-inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Confirmar')">Borrar</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
-@endif
-
-
-<a href="{{ url('employee/create') }}" class="btn btn-primary mb-2">Registrar empleado</a>
-
-<table class="table table-bordered">
-    <thead class="thead-light">
-        <tr>
-            <th>Id</th>
-            <th>Username</th>
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>Rol</th>
-            <th>Acciones</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($employees as $employee)
-            <tr>
-                <td>{{ $employee->id }}</td>
-                <td>{{ $employee->username }}</td>
-                <td>{{ $employee->name }}</td>
-                <td>{{ $employee->last_name }}</td>
-                <td>{{ \App\Enums\UserRole::getDescription($employee->role->value) }}</td>
-                <td>
-                    <a href="{{ url('employee/'.$employee->id.'/edit') }}" class="btn btn-sm btn-warning">Editar</a>
-                    <form action="{{ route('employee.destroy', $employee->id) }}" method="post" class="d-inline">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Confirmar')">Borrar</button>
-                    </form>
-                </td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
-
-
-    <!-- Scripts de Bootstrap  -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
+    @include('layouts.scripts')
 </body>
 </html>
