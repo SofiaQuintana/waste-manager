8d00ed5c0405086a135d5046256b2249b53e6cc9
SofiaQuintana <sofiaquintana2@gmail.com>
2023-11-14 14:19:05 -0600

    fix: ui styling and template

--- resources/views/layouts/options.blade.php
+++ resources/views/layouts/options.blade.php
@@ -7,11 +7,17 @@
             <span class="item">Registrar empleado</span>
         </a></li>
     @elseif($employeeData['roleDescription'] === 'WasteClassifier')
-        <li><a href="{{ url('employee.index') }}">
-            <span class="item">Principal</span>
+        <li><a href="{{ url('home') }}">
+            <span class="item">Lista de Residuos</span>
         </a></li>
-        <li><a href="{{ url('employee/create') }}">
-            <span class="item">Registrar empleado</span>
+        <li><a href="{{ url('waste-inventory/create') }}">
+            <span class="item">Registrar Residuo</span>
+        </a></li>
+        <li><a href="{{ route('waste-income.index') }}">
+            <span class="item">Lista de Ingresos</span>
+        </a></li>
+        <li><a href="{{ url('waste-inventory.create') }}">
+            <span class="item">Registrar Ingreso</span>
         </a></li>
     @elseif($employeeData['roleDescription'] === 'Operator')
         <li><a href="{{ url('operator/index') }}">
@@ -24,11 +30,5 @@
         <li><a href="/resources//views/Reportes.php">
             <span class="item">Reportes</span>
         </a></li>
-
-    @endif
-    <li><a href="/resources//views/inventario.php">
-        <span class="item">Ver inventario</span>
-    </a></li>
-   
-   
+    @endif  
 </ul>
